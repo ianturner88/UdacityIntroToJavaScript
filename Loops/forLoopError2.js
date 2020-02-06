491181293b535666d3cd4bc711c4310a7050e82a
Ian Turner <ianturner88@gmail.com>
2020-02-06 18:01:03 -0500

    2nd for loop error fixed

---
+++ Loops/forLoopError2.js
@@ -0,0 +1,12 @@
+/*
+Directions:
+The for loop below has an error. Fix it!
+
+for (var k = 0 k < 200 k++) {
+    console.log(k);
+    }
+*/
+
+for (var k = 0; k < 200; k++) {
+    console.log(k);
+  }
