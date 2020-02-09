9fd08ad1d65cc710b3bc00d0a7847f3ca4f49956
Ian Turner <ianturner88@gmail.com>
2020-02-08 19:23:55 -0500

    Intro to Arrays

---
+++ Arrays/UdaciFamily.js
@@ -0,0 +1,10 @@
+/* 
+Directions:
+Create an array called udaciFamily and add "Julia", "James", and your name to the array. Then, print the udaciFamily to the console using console.log.
+*/
+
+let udaciFamily = [];
+
+udaciFamily.push("Julia");
+udaciFamily.push("James");
+udaciFamily.push("Ian");
--- Functions/quiz22.js
+++ Functions/quiz22.js
@@ -6,23 +6,6 @@ emotions("happy", laugh(2)); // you can use your laugh function from the previou
 Prints: "I am happy, haha!"
 */
 
-/*
-// Function expression that assigns the function displayFavorite 
-// to the variable favoriteMovie
-var favoriteMovie = function displayFavorite(movieName) {
-  console.log("My favorite movie is " + movieName);
-};
-
-// Function declaration that has two parameters: a function for displaying
-// a message, along with a name of a movie
-function movies(messageFunction, name) {
-  messageFunction(name);
-}
-
-// Call the movies function, pass in the favoriteMovie function and name of movie
-movies(favoriteMovie, "Finding Nemo");
-*/
-
 var favoriteMovie = function displayFavorite(movieName) {
     console.log("My favorite movie is " + movieName);
   };
