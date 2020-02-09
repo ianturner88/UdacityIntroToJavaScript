89a323ddce42055b224481ed6bc3b87715e810bd
Ian Turner <ianturner88@gmail.com>
2020-02-09 16:01:11 -0500

    Array reversed

---
+++ Arrays/checkingOutTheDocs.js
@@ -0,0 +1,17 @@
+/* 
+Use the MDN Documentation to determine which of these methods would be best for reversing elements in this array:
+
+var reverseMe = ["h", "e", "l", "l", "o"];
+*/
+
+//starting array
+let reverseMe = ["h", "e", "l", "l", "o"];
+
+console.log("Not reversed: ");
+console.log(reverseMe);
+
+//reverse the array
+reverseMe.reverse();
+
+console.log("Reversed: ");
+console.log(reverseMe)
