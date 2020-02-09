782e7f25d70f939dba6da76ffe23a979a1e9bb01
Ian Turner <ianturner88@gmail.com>
2020-02-08 19:32:53 -0500

    Intro to Arrays complete

--- Arrays/UdaciFamily.js
+++ Arrays/UdaciFamily.js
@@ -3,8 +3,21 @@ Directions:
 Create an array called udaciFamily and add "Julia", "James", and your name to the array. Then, print the udaciFamily to the console using console.log.
 */
 
+console.log("START");
+
 let udaciFamily = [];
 
 udaciFamily.push("Julia");
 udaciFamily.push("James");
 udaciFamily.push("Ian");
+
+function printArrayContents(udaciFamily) {
+    for (let counter = 0; counter < udaciFamily.length; counter++) {
+        //outputs the contents of the array
+        console.log(udaciFamily[counter]);
+    }
+}
+
+printArrayContents(udaciFamily);
+
+console.log("END");
