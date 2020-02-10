fd0a0b6fb057df7c3c758cf38b27686cba861397
Ian Turner <ianturner88@gmail.com>
2020-02-10 09:28:11 -0500

    Another type of loop complete

--- Arrays/anotherTypeOfLoop.js
+++ Arrays/anotherTypeOfLoop.js
@@ -6,24 +6,20 @@ var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 
 Remember that the "Test Run" button will display any logged content, so feel free to use console.log() to test your code.
 */
 
-/* 
-EXAMPLE:
-const arraySparse = [1,3,,7]
-let numCallbackRuns = 0
-
-arraySparse.forEach(function(element){
-  console.log(element)
-  numCallbackRuns++
-})
-
-console.log("numCallbackRuns: ", numCallbackRuns)
-*/
-
+console.log("START --");
 
 let test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139];
 
+//original array
+console.log(test);
+
 function isDivisibleByThree (item, index, arr) {
+  /* add 100 to each of the values if the value is divisible by 3 */
     if ((item % 3) === 0) {
-        arr[index] = item * 100;
+        arr[index] = item + 100;
     }
 }
+
+test.forEach(isDivisibleByThree);
+
+console.log(test);
