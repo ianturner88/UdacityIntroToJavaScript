170b7b87a677478c7753f39d12b989b07fe3577a
Ian Turner <ianturner88@gmail.com>
2020-02-13 11:42:15 -0500

    Bank Account complete

--- Objects/bankAccounts.js
+++ Objects/bankAccounts.js
@@ -25,23 +25,24 @@ Your balance is currently $1000 and your interest rate is 1%.
 
 console.log("START");
 
-var savingsAccount = {
-    balance: 1000,
-    interestRatePercent: 1,
-    deposit: function addMoney(amount) {
-      if (amount > 0) {
-        savingsAccount.balance += amount;
-      }
-    },
-    withdraw: function removeMoney(amount) {
-      var verifyBalance = savingsAccount.balance - amount;
-      if (amount > 0 && verifyBalance >= 0) {
-        savingsAccount.balance -= amount;
-      }
-    },  
-    summary: function printAccountSummary() {
-        console.log("Welcome! \n Your balance is currently $1000 and your interest rate is 1%.")
-    },
+let savingsAccount = {
+  balance: 1000,
+  interestRatePercent: 1,
+  deposit: function addMoney(amount) {
+    if (amount > 0) {
+      savingsAccount.balance += amount;
+    }
+  },
+  withdraw: function removeMoney(amount) {
+    var verifyBalance = savingsAccount.balance - amount;
+    if (amount > 0 && verifyBalance >= 0) {
+      savingsAccount.balance -= amount;
+    }
+  },
+  print: function printAccountSummary() {
+    console.log("Welcome! \n Your balance is currently $1000 and your interest rate is 1%.");
+  }
 };
 
-console.log(savingsAccount.removeMoney(100));
+console.log(savingsAccount.withdraw(100));
+console.log(savingsAccount.print());
